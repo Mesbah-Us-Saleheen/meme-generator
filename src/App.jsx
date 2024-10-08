3bcfeb71dca60c2930e1a3bd7e5b624f95865e20
mesbah <mesbahsaleheen@yahoo.com>
2024-10-08 22:11:56 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
+import './App_custom.css'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return (
+		<header className="header">
+			<img src="img/troll-face.png" alt="" className="header--image" />
+			<h2 className="header--title">Meme Generator</h2>
+			<h4 className="header--project">React Course - Project</h4>
+		</header>
+	)
+}
